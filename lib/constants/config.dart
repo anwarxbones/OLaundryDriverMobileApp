7285c0bd4130a09d43fe032dc679eb922a4c94e7
rajuislamdev <rajuislam.dev@gmail.com>
2025-01-28 10:47:50 +0600

    Add production base url and development base url

--- lib/constants/config.dart
+++ lib/constants/config.dart
@@ -1,5 +1,9 @@
 class AppConfig {
   AppConfig._();
+  // Production
+  // static const String baseUrl = 'https://prod.olaundry.com/api';
+  // Development
   static const String baseUrl = 'http://161.35.188.114/api';
+
   // static const String baseUrl = 'http://olaundry.local/api';
 }
